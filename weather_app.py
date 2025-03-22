198469072b4a719ee3a4d23259b7f7143f029ab3
Steven Wang <stev.wang@mail.utoronto.ca>
2025-03-22 11:37:00 -0400

    Added Basic tkinter GUI

--- weather_app.py
+++ weather_app.py
@@ -1,14 +1,49 @@
-# import module
-from geopy.geocoders import Nominatim
+# Name: Steven Wang
 
-# initialize Nominatim API (Has trouble with Canadian Postal code)
-location = Nominatim(user_agent="weather_app")
+import requests
+import tkinter as tk
+from tkinter import messagebox
 
-coord = location.geocode("Erindale Park, Canada", exactly_one=True)
- 
-# printing address
-print(coord.address)
- 
-# printing latitude and longitude
-print("Latitude = ", coord.latitude, "\n")
-print("Longitude = ", coord.longitude)
+import geocoder
+
+# Get Current User Location (Warning, using ip, may narrow only to city)
+def get_curr_loc():
+    curr_loc = geocoder.ip("me")
+    if curr_loc.latlng:
+        latitude, longitude = curr_loc.latlng
+        address = curr_loc.address
+        return latitude, longitude, address
+    else:
+        return None
+
+# Create the main window
+root = tk.Tk()
+root.title("Weather App")
+
+root.eval('tk::PlaceWindow . center')
+
+# Create label and entry field
+location_label = tk.Label(root, text="Location:")
+location_label.grid(row=0)
+location_entry = tk.Entry(root)
+location_entry.grid(row=0, column=1)
+
+curr_loc = get_curr_loc()
+if curr_loc != None:
+    latitude, longitude, address = curr_loc
+else:
+    latitude, longitude, address = None
+
+# Create a label to display Location information
+Location_label = tk.Label(root, text=f"Finding Weather for: {address}")
+Location_label.grid(row=2, columnspan=2)
+
+# Create a label to display weather information
+weather_label = tk.Label(root, text="")
+weather_label.grid(row=3, columnspan=2)
+
+# Create a button to find weather data (Default to current Location)
+weather_button = tk.Button(root, text="See Weather")
+weather_button.grid(row=1, columnspan=2)
+
+root.mainloop()
