5c761ecdb9c6f6d5fe95ee63bbbc84be9de5fc8e
Steven Wang <stev.wang@mail.utoronto.ca>
2025-03-23 23:45:37 -0400

    finished basic functionality

--- weather_app.py
+++ weather_app.py
@@ -1,5 +1,5 @@
 # Name: Steven Wang
-
+# Disclaimer, the API can only be used 10,000 times each day
 
 import tkinter as tk
 from tkinter import ttk
@@ -10,9 +10,16 @@ import geocoder
 import requests
 import json
 
+import openmeteo_requests
+from openmeteo_sdk.Variable import Variable
+
+import numpy as np
+
+# Global Variables
+openmeteo = openmeteo_requests.Client()
 pred_items = []
 pred_items_verbose = []
-latitude, longitude, address = None, None, None
+latitude, longitude= None, None
 
 # Get Current User Location (Warning, using ip, may narrow only to city)
 # Returns latitude, longitude, address
@@ -24,13 +31,18 @@ def get_curr_loc():
         return lat, long, addr
     else:
         return None, None, None
-    
+
+# Get the latitude, longitude and address from country json data
+# Returns latitude, longitude and address
 def get_loc_data(country):
     lat = country['latitude']
     long = country['longitude']
-    addr = f"{country['name']} {country['country_code']}"
+    addr = f"{country['name']} {country.get('country_code', '')}"
     return lat, long, addr
-    
+
+# get the top 5 predicted locations from text in text field
+# using open-meteo geocoding api
+# returns readable country names, and their json data counter parts
 def get_pred(text):
     if len(text) <= 1: 
         return [], []
@@ -43,14 +55,15 @@ def get_pred(text):
             data = response.json()
             predictions = data.get("results", [])
             if predictions != []:
-                predictions_readable = [f"{guess['name']}, {guess['country']}" for guess in predictions]
+                predictions_readable = [f"{guess['name']}, {guess.get('country', '')}" for guess in predictions]
             else:
                 predictions_readable = []
             return predictions_readable, predictions
                 
         else:
             return None, None
-    
+
+# Update the listbox we use for prediction displays
 def update_pred(lst):
     # clear previous data 
     pred_lst.delete(0, 'end') 
@@ -58,7 +71,8 @@ def update_pred(lst):
     # put new data 
     for item in lst: 
         pred_lst.insert('end', item) 
-        
+
+# Set the entry box text to the selected name in the listbox
 def set_loc_text(text):
     location_entry.delete(0,END)
     location_entry.insert(0,text)
@@ -67,8 +81,9 @@ def set_loc_text(text):
     update_pred(pred_items)
     return
         
+# Event handler for when a name in the list box is selected
 def select_pred(event):
-    global latitude, longitude, address
+    global latitude, longitude
     
     if not event.widget.curselection():
         return
@@ -78,15 +93,129 @@ def select_pred(event):
     print(latitude, longitude)
     set_loc_text(pred_items[ind])
     location_label.config(text=f"Finding Weather for: {address}")
-    
+
+# Event handler for search mode, calls geocoding API
 def pred(event):
-    if not event.widget.get():
-        return
-    if var1.get():
+    if not event.widget.get() or var1.get():
         return
     txt = event.widget.get()
     pred_items[:], pred_items_verbose[:] = get_pred(txt)
     update_pred(pred_items)
+
+# Event handler for coordinate mode, parses input for coordinate
+def coord_parse(event):
+    if not event.widget.get() or not var1.get():
+        return
+    txt = event.widget.get()
+    try:
+        location_label.config(text=f"Finding Weather for: {txt}")
+        global latitude, longitude
+        str_lat, str_long = txt.split(", ")
+        latitude, longitude = (float(str_lat), float(str_long))
+        print(latitude, longitude)
+    except:
+        location_label.config(text="Invalid Coordinate!")
+        
+def get_weather():
+    url = "https://api.open-meteo.com/v1/forecast"
+    params = {
+        "latitude": latitude,
+        "longitude": longitude,
+        "daily": ["weather_code", "temperature_2m_max", "temperature_2m_min", "precipitation_probability_max"],
+        "current": ["temperature_2m", "precipitation_probability", "weather_code", "apparent_temperature", "relative_humidity_2m"],
+        "timezone": "auto",
+        "forecast_days": 5
+    }
+    responses = openmeteo.weather_api(url, params=params)
+    response = responses[0]
+    
+    current = response.Current()
+    
+    curr_temp = round(current.Variables(0).Value(), 2)
+
+    curr_prec_prob = int(current.Variables(1).Value())
+
+    curr_weather_code = int(current.Variables(2).Value())
+
+    curr_apparent_temp = round(current.Variables(3).Value(), 2)
+    
+    curr_relative_humidity = int(current.Variables(4).Value())
+    
+    label, symbol = weather_code_translate(curr_weather_code)
+    
+    weather_label.config(text=label)
+    weather_symbol.config(text=symbol)
+    
+    temperature.config(text=f"Temperature: {curr_temp}°C")
+    
+    ap_temperature.config(text=f"Apparent Temperature: {curr_apparent_temp}°C")
+    
+    rain_chance.config(text=f"Chance of Rain: {curr_prec_prob}%")
+    
+    humidity.config(text=f"Humidity: {curr_relative_humidity}%")
+
+    pass
+
+def weather_code_translate(code):
+    match code:
+        case 0:
+            return "Clear sky", "☀️"
+        case 1:
+            return "Mainly clear", "🌤️"
+        case 2:
+            return "partly cloudy", "🌥️"
+        case 3:
+            return "Overcast", "☁️"
+        case 45:
+            return "fog", "🌫️"
+        case 48:
+            return "Depositing rime fog", "🌫️"
+        case 51:
+            return "Light drizzle", "🌧️"
+        case 53:
+            return "Moderate drizzle", "🌧️"
+        case 55:
+            return "Dense drizzle", "🌧️"
+        case 56:
+            return "Light freezing drizzle", "🌨️"
+        case 57:
+            return "Dense freezing drizzle", "🌨️"
+        case 61:
+            return "Slight rain", "🌦️"
+        case 63:
+            return "Moderate rain", "🌧️"
+        case 65:
+            return "Heavy rain", "🌧️"
+        case 66:
+            return "Light freezing rain", "🌨️"
+        case 67:
+            return "Heavy freezing rain", "🌨️"
+        case 71:
+            return "Slight snow", "❄️"
+        case 73:
+            return "Moderate snow", "❄️"
+        case 75:
+            return "Heavy snow", "❄️"
+        case 77:
+            return "Snow grains", "❄️"
+        case 80:
+            return "Slight rain showers", "🌦️"
+        case 81:
+            return "Moderate rain showers", "🌧️"
+        case 82:
+            return "Violent rain showers", "🌧️"
+        case 85:
+            return "Light snow showers", "❄️"
+        case 86:
+            return "Heavy snow showers", "❄️"
+        case 95:
+            return "Slight/Moderate thunderstorm", "🌩️"
+        case 96:
+            return "Thunderstorm with slight hail", "⛈️"
+        case 99:
+            return "Thunderstorm with heavy hail", "⛈️"
+        case _:
+            return "unknown", "🤷‍♂️"
 
 
 # Find current coordinate and Location
@@ -102,8 +231,9 @@ location_label.grid(row=0, column=0)
 location_entry = tk.Entry(root, width=35)
 location_entry.grid(row=0, column=1, columnspan=3)
 location_entry.bind('<KeyRelease>', pred) 
+location_entry.bind('<Return>', coord_parse)
 
-# Create a listbox widget
+# Create a listbox widget to house predicted names
 pred_lst = Listbox(root, width=35, height=5, selectmode="single")
 pred_lst.grid(row=1, column=1, columnspan=3)
 pred_lst.bind("<<ListboxSelect>>", select_pred)
@@ -122,12 +252,30 @@ else:
     location_label.config(text=f"Finding Weather for: {address}")
     
 # Create a button to find weather data (Default to current Location)
-weather_button = tk.Button(root, text="See Weather")
+weather_button = tk.Button(root, text="See Weather", command=get_weather)
 weather_button.grid(row=3, columnspan=5)
 
-# Create a label to display weather information
+separator_1 = ttk.Separator(root, orient='horizontal')
+separator_1.grid(row=4, columnspan=5, sticky="ew")
+
+# Create labels to display weather information
 weather_label = tk.Label(root, text="")
-weather_label.grid(row=4, columnspan=5)
+weather_label.grid(row=5, columnspan=5)
+
+weather_symbol = tk.Label(root, text="", font=("Segoe UI Emoji", 50))
+weather_symbol.grid(row=6, columnspan=5)
+
+temperature = tk.Label(root, text="")
+temperature.grid(row=9, column=0, columnspan=2)
+
+ap_temperature = tk.Label(root, text="")
+ap_temperature.grid(row=9, column=3, columnspan=2)
+
+rain_chance = tk.Label(root, text="")
+rain_chance.grid(row=10, column=0, columnspan=2)
+
+humidity = tk.Label(root, text="")
+humidity.grid(row=10, column=3, columnspan=2)
 
 root.eval('tk::PlaceWindow . center')
 
